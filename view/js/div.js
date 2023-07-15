f198fa1823bf7c43e0568c0372b0be0611a01893
ByteBoss <kienntps27053@fpt.edu.vn>
2023-07-15 10:41:11 +0700

    kin615223

---
+++ view/js/div.js
@@ -0,0 +1,28 @@
+    ScrollReveal({
+    distance: '65px',
+    duration: '2500',
+    delay: '450',
+    reset: true
+
+});
+//ve chung toi
+ScrollReveal().reveal('.conten-font-main', { delay: 200,origin:'left'});
+ScrollReveal().reveal('.conten-font-top', { delay: 500,origin:'top'});
+ScrollReveal().reveal('.conten-font-mid', { delay: 500,origin:'bottom'});
+ScrollReveal().reveal('.conten-font-mid-1', { delay: 500,origin:'bottom'});
+ScrollReveal().reveal('.conten-font-bottom', { delay: 500,origin:'bottom'});
+ScrollReveal().reveal('.conten-all', { delay: 500,origin:'right'});
+//tour co ban
+ScrollReveal().reveal('.main-h1', { delay: 200, });// origin: 'top'
+// tour noi bat
+ScrollReveal().reveal('.main-container-mb', { delay: 200, origin: 'bottom' });
+ScrollReveal().reveal('.main-container-mb-h1', { delay: 350, origin: 'top' });
+// tour yeu thich
+ScrollReveal().reveal('.tf-container', { delay: 350, origin: 'bottom' });
+ScrollReveal().reveal('.tf-h2', { delay: 400, origin: 'top' });
+ScrollReveal().reveal('.tf-h1', { delay: 700, origin: 'left' });
+ScrollReveal().reveal('.tf-time', { delay: 400, origin: 'right' });
+ScrollReveal().reveal('.tf-input', { delay: 350, origin: 'bottom' });
+// /////////////////
+
+
---
+++ view/js/header.js
@@ -0,0 +1,5 @@
+window.addEventListener("scroll", function()
+{
+    const header = document.querySelector("nav");
+    header.classList.toggle("sticky",window.scrollY>0);
+});
