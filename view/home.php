ccc9ed3860a7c3e3496df355fb92c712458a7e1b
ByteBoss <kienntps27053@fpt.edu.vn>
2023-07-27 10:00:55 +0700

    kin

--- view/home.php
+++ view/home.php
@@ -1,8 +1,9 @@
 <?php
-    include "./view/view-control/baner.php";
-    include "./view/view-control/gioithieu.php";
-    include "./view/view-control/danhmuc.php";
-    include "./view/view-control/noibat.php";
-    include "./view/view-control/yeuthich.php";
-    include "./view/view-control/tintuc.php";
+    // include "./view/view-control/baner.php";
+    // include "./view/view-control/gioithieu.php";
+    // include "./view/view-control/danhmuc.php";
+    // include "./view/view-control/noibat.php";
+    // include "./view/view-control/yeuthich.php";
+    // include "./view/view-control/tintuc.php";
+    include "./view/view-control/tintuc-ct.php";
 ?>
