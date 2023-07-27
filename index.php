78d6163a3a72ecf92bbef9d25837c018f4feb42b
BinhPeo <138087577+BinhPeo@users.noreply.github.com>
2023-07-27 18:19:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,50 @@
 <?php
+
  include "view/header.php";
  if ((isset($_GET['act'])) && ($_GET['act'] != "")) {
      $act = $_GET['act'];
      switch ($act) {
+
+
+
+
+
+        // bình
+
+
+
+
+
          case 'about':
              include "/view/about.php";   
              break;
+        
+
+
+
+
+
+
+        // huy
+
+
+
+
+
+
+
+
+
+
+
+        // quân
+
+
+
+
+
+
+
          default:
              include "view/home.php";
              break;
